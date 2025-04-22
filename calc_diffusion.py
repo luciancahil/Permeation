e8577a561dc2f8ee0dbf703c7add316d5b26ea91
luciancahil <johndoe@example.com>
2025-04-22 12:57:18 -0700

    fixed diffusion calculation

--- calc_diffusion.py
+++ calc_diffusion.py
@@ -28,11 +28,10 @@ def read_block(file):
         info = [float(num) for num in atom_line.split(" ")]
         atoms_info.append(info)
     
-    # sort by ID
-    sorted(atoms_info, key = lambda info: info[0])
 
-    # sort by type
-    sorted(atoms_info, key = lambda info: info[1])
+    # sort by molecular id, then atom ID, then atom type
+    atoms_info.sort(key=lambda info: (info[-1], info[0], info[1]))
+
 
     return step_num, atoms_info, lengths
 
@@ -44,9 +43,9 @@ def plot_hist(list, name):
     plt.close()
 
 
-log_file = open("./logs/diffuse.txt", mode='r')
+log_file = open("./logs/diffuseMix.txt", mode='r')
 
-coord_file = open("./outputs/permanent/penetrant_C92x5x5.data_CH4.lmp_diffusion.xyz")
+coord_file = open("./outputs/permanent/penetrant_C92x5x5.data_H2.lmp_2_diffusion.xyz")
 
 minimization_stoppages = []
 
@@ -73,7 +72,6 @@ while(line):
     step_num, atoms_info, lengths = read_block(coord_file)
 
     if(step_num == jiggling_stopping_steps[cycle]):
-        
         if(cycle % 100 == 0):
             print(cycle)
         cycle += 1
@@ -93,7 +91,6 @@ while(line):
     line = coord_file.readline()
 
 
-breakpoint()
 x_coords = []
 y_coords = []
 z_coords = []
@@ -125,9 +122,9 @@ for cycle in cycles_info:
         z_diff.append(round[2] - prev_step[2])
     
 
-    x_diff = [num for num in x_diff if abs(num) < box_lenths[0] / 2]
-    y_diff = [num for num in y_diff if abs(num) < box_lenths[1] / 2]
-    z_diff = [num for num in z_diff if abs(num) < box_lenths[2] / 2]
+    x_diff = [num*num for num in x_diff if abs(num) < box_lenths[0] / 2]
+    y_diff = [num*num for num in y_diff if abs(num) < box_lenths[1] / 2]
+    z_diff = [num*num for num in z_diff if abs(num) < box_lenths[2] / 2]
 
     if(x_diff == [] or y_diff == [] or z_diff == []):
         continue
@@ -143,19 +140,23 @@ for cycle in cycles_info:
 
 # So what do I do?....
 
+
+# add axes and title.
+
+# write a function
 plt.scatter(range(len(x_MSD)), x_MSD)
-plt.savefig('./pics/x_MSD.png')
+plt.savefig('./pics/H2_mix_x_MSD.png')
 plt.close()
 plt.scatter(range(len(y_MSD)), y_MSD)
-plt.savefig('./pics/y_MSD.png')
+plt.savefig('./pics/H2_mix_y_MSD.png')
 plt.close()
 plt.scatter(range(len(z_MSD)), z_MSD)
-plt.savefig('./pics/y_MSD.png')
+plt.savefig('./pics/H2_mix_z_MSD.png')
 plt.close()
 
-plot_hist(x_coords, "x_coords")
-plot_hist(y_coords, "y_coords")
-plot_hist(z_coords, "z_coords")
+plot_hist(x_coords, "H2_mix_x_coords")
+plot_hist(y_coords, "H2_mix_mixy_coords")
+plot_hist(z_coords, "H2_mix_mixz_coords")
 
 avg_x = sum(x_MSD) / len(x_MSD)
 avg_y = sum(y_MSD) / len(y_MSD)
@@ -167,3 +168,19 @@ print("Y Diffusion Coef: {}".format(avg_y * avg_y / 2))
 print("Z Diffusion Coef: {}".format(avg_z * avg_z / 2))
 
 print("Done!")
+
+
+
+"""
+H2 alone:
+
+X Diffusion Coef: 0.8141761574435478
+Y Diffusion Coef: 0.6532706633776124
+Z Diffusion Coef: 0.851524547248021
+
+H2 with CH4:
+
+X Diffusion Coef: 0.7508528365061569
+Y Diffusion Coef: 0.6074194124677061
+Z Diffusion Coef: 1.0314104937235975
+"""
