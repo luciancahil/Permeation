cff0a0d2f09d5b194dd0f1c9c2f7fd4bbf065b16
luciancahil <johndoe@example.com>
2025-05-06 12:50:05 -0700

    added distance calculation to diffusion

--- calc_diffusion.py
+++ calc_diffusion.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+import statistics
 def read_block(file):
     # first dummy line
     step_num = int(file.readline())
@@ -18,7 +20,7 @@ def read_block(file):
         lengths.append(length)
     
     file.readline()
-    # info: 'ITEM: ATOMS id type x y z vx vy vz fx fy fz\n'
+    # info: 'ITEM: ATOMS id type x y z vx vy vz fx fy fz molecule\n'
 
     # read atom info:
     atoms_info = []
@@ -46,6 +48,8 @@ def plot_hist(list, name):
 log_file = open("./logs/diffuseMix.txt", mode='r')
 
 coord_file = open("./outputs/permanent/penetrant_C92x5x5.data_H2.lmp_2_diffusion.xyz")
+file_type_name = "H2_mix_"
+
 
 minimization_stoppages = []
 
@@ -66,24 +70,38 @@ line = coord_file.readline()
 
 cycles_info = []
 box_lenths = []
-
+distances = []
+# stores a 3x100 matrix, where each row is the coords of the main atom at a given time step.
 new_cycle = []
+distance_added = False
 while(line):
     step_num, atoms_info, lengths = read_block(coord_file)
 
     if(step_num == jiggling_stopping_steps[cycle]):
+        # finished a round. Reset everything
         if(cycle % 100 == 0):
             print(cycle)
         cycle += 1
         cycles_info.append(new_cycle)
         new_cycle = []
         box_lenths = lengths
+        distance_added = False
 
         if(cycle == len(minimization_stoppages)):
             break
     
     if(step_num >= minimization_stoppages[cycle]):
+        # add in coordinate info of atoms
         main_atom = atoms_info[0][2:5]     
+
+        if(not distance_added):
+            aux_atom = atoms_info[2][2:5]
+
+            distance = sum([(main_atom[i] - aux_atom[i])**2 for i in range(3)])**(1/2)
+            distances.append(distance)
+
+            distance_added = True
+
         new_cycle.append(main_atom)
 
     
@@ -99,6 +117,7 @@ z_coords = []
 x_MSD = []
 y_MSD = []
 z_MSD = []
+total_MSD = []
 
 # look at 1
 
@@ -120,17 +139,22 @@ for cycle in cycles_info:
         x_diff.append(round[0] - prev_step[0])
         y_diff.append(round[1] - prev_step[1])
         z_diff.append(round[2] - prev_step[2])
-    
+
+
+        prev_step = round
 
     x_diff = [num*num for num in x_diff if abs(num) < box_lenths[0] / 2]
     y_diff = [num*num for num in y_diff if abs(num) < box_lenths[1] / 2]
     z_diff = [num*num for num in z_diff if abs(num) < box_lenths[2] / 2]
 
     if(x_diff == [] or y_diff == [] or z_diff == []):
+        breakpoint()
         continue
+        
     x_MSD.append(sum(x_diff) / len(x_diff))
     y_MSD.append(sum(y_diff) / len(y_diff))
     z_MSD.append(sum(z_diff) / len(z_diff))
+
 
 
 # [16.715041999676394, 12.957574372484338, 90.70302060737704]
@@ -138,49 +162,70 @@ for cycle in cycles_info:
 
 # Okay, we have a problem. The box dims actually vary quite a lot here, unfortunately.
 
-# So what do I do?....
+# So what do I do?
+
+# just drop all the atoms.
 
 
 # add axes and title.
 
 # write a function
+
 plt.scatter(range(len(x_MSD)), x_MSD)
-plt.savefig('./pics/H2_mix_x_MSD.png')
+plt.savefig('./pics/{}x_MSD.png'.format(file_type_name))
 plt.close()
 plt.scatter(range(len(y_MSD)), y_MSD)
-plt.savefig('./pics/H2_mix_y_MSD.png')
+plt.savefig('./pics/{}y_MSD.png'.format(file_type_name))
 plt.close()
 plt.scatter(range(len(z_MSD)), z_MSD)
-plt.savefig('./pics/H2_mix_z_MSD.png')
+plt.savefig('./pics/{}z_MSD.png'.format(file_type_name))
 plt.close()
 
-plot_hist(x_coords, "H2_mix_x_coords")
-plot_hist(y_coords, "H2_mix_mixy_coords")
-plot_hist(z_coords, "H2_mix_mixz_coords")
+plot_hist(x_coords, "{}x_coords".format(file_type_name))
+plot_hist(y_coords, "{}y_coords".format(file_type_name))
+plot_hist(z_coords, "{}z_coords".format(file_type_name))
 
 avg_x = sum(x_MSD) / len(x_MSD)
 avg_y = sum(y_MSD) / len(y_MSD)
 avg_z = sum(z_MSD) / len(z_MSD)
 
+diffusion_file = open("{}_diffusions.csv".format(file_type_name), mode="w")
+diffusion_file.write("{}\n{}\n{}\n".format(x_MSD,y_MSD, z_MSD))
 
-print("X Diffusion Coef: {}".format(avg_x * avg_x / 2))
-print("Y Diffusion Coef: {}".format(avg_y * avg_y / 2))
-print("Z Diffusion Coef: {}".format(avg_z * avg_z / 2))
 
+
+print("X Diffusion Coef: {}".format(avg_x / 2))
+print("X Diffuseion stdev: {}".format(statistics.stdev(x_MSD)))
+print("Y Diffusion Coef: {}".format(avg_y / 2))
+print("Y Diffuseion stdev: {}".format(statistics.stdev(y_MSD)))
+print("Z Diffusion Coef: {}".format(avg_z / 2))
+print("Z Diffuseion stdev: {}".format(statistics.stdev(z_MSD)))
+print("Total Diffusion: ")
+print("Num samples: {}".format(len(x_MSD)))
+
+file = ""
 print("Done!")
 
 
 
 """
-H2 alone:
 
-X Diffusion Coef: 0.8141761574435478
-Y Diffusion Coef: 0.6532706633776124
-Z Diffusion Coef: 0.851524547248021
+Units: Angstroms ^2 / fs
+H2 alone:
+X Diffusion Coef: 3.5868667597388445e-05
+Y Diffusion Coef: 3.60909780024294e-05
+Z Diffusion Coef: 3.7740240479389096e-05
+Diffusion Coef:   3.656662869306898e-05
 
 H2 with CH4:
 
-X Diffusion Coef: 0.7508528365061569
-Y Diffusion Coef: 0.6074194124677061
-Z Diffusion Coef: 1.0314104937235975
+X Diffusion Coef: 3.297382266605165e-05
+Y Diffusion Coef: 3.3248013049559864e-05
+Z Diffusion Coef: 3.52204368200141e-05
+Diffusion Coef:   3.381409084520854e-05
+
+CH4 with H2:
+X Diffusion Coef: 4.3388338049547655e-06
+Y Diffusion Coef: 4.41920272674957e-06
+Z Diffusion Coef: 5.563779518824443e-06
 """
